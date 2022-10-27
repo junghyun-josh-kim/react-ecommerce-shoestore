1157dabbb319316a8100198f9c7b6e99d8a2a5cc
Josh-Kim-Toronto <jhkim911206@gmail.com>
2022-10-26 20:54:57 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { Row, Col, Navbar, Container, Nav } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar bg='light' variant='light'>
+        <Container className='navbar'>
+          <Navbar.Brand href='#home' className='logo'>
+            Josh's Shoes
+          </Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='#home'>Home</Nav.Link>
+            <Nav.Link href='#features'>Features</Nav.Link>
+            <Nav.Link href='#pricing'>Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg'></div>
+      <Container>
+        <Row>
+          <Col>
+            <img src='/img/shoe-001.jpg' width='80%' />
+            <h4>Name</h4>
+            <p>Description</p>
+          </Col>
+          <Col>
+            <img src='/img/shoe-002.jpg' width='80%' />
+            <h4>Name</h4>
+            <p>Description</p>
+          </Col>
+          <Col>
+            <img src='/img/shoe-003.jpg' width='80%' />
+            <h4>Name</h4>
+            <p>Description</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
